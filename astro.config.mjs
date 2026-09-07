26e0b9318103298ea257ee136eb71c15f0e56275
Jeremy Zhou <thejeremyzhou@gmail.com>
2026-09-07 22:50:23 +0800

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,34 +5,33 @@ import { unified } from '@astrojs/markdown-remark';
 import starlight from '@astrojs/starlight';
 import react from '@astrojs/react';
 import starlightThemeFlexoki from 'starlight-theme-flexoki'
+import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
-	markdown: {
-		processor: unified(),
-	},
-	integrations: [
-		starlight({
-			plugins: [
-				starlightThemeFlexoki({ accentColor: 'orange' }),
-				starlightKatex(),
-			],
-			title: 'LaundromatCat',
-			customCss: [
-				'./src/styles/hero.css',
-				'./src/styles/colors.css'
-			],
-			components: {
-				PageTitle: './src/components/PageTitle.astro',
-			},
-			sidebar: [
+    site: 'https://laundromatcat.pages.dev',
+    markdown: {
+        processor: unified(),
+    },
+    integrations: [starlight({
+        plugins: [
+            starlightThemeFlexoki({ accentColor: 'orange' }),
+            starlightKatex(),
+        ],
+        title: 'LaundromatCat',
+        customCss: [
+            './src/styles/hero.css',
+            './src/styles/colors.css'
+        ],
+        components: {
+            PageTitle: './src/components/PageTitle.astro',
+        },
+        sidebar: [
 
-				{
-					label: 'Posts',
-					items: [{ autogenerate: { directory: 'posts' } }],
-				},
-			],
-			favicon: '/src/assets/favicon.png',
-		}),
-		react(),
-	],
+            {
+                label: 'Posts',
+                items: [{ autogenerate: { directory: 'posts' } }],
+            },
+        ],
+        favicon: '/src/assets/favicon.png',
+		}), react(), sitemap()],
 });
